a421ddfad4452b4a9ebc1b83bcfae14741349520
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-02-26 11:18:13 +0330

    Create bank data class for keep data.

---
+++ src/main/java/repository/BankData.java
@@ -0,0 +1,11 @@
+package repository;
+
+import entity.BankAccount;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BankData {
+
+    public static Map<String, BankAccount> accounts = new HashMap<>();
+}
