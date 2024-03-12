70de656fb59a3916fa4f2d3db5a18393c6ba9127
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-03-12 12:05:49 +0330

    delete interfaces folder.

--- src/main/java/interfaces/Fees.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import java.util.Date;
-
-public interface Fees {
-
-    void deductFees(double amount);
-    void displayTotalMonthlyFees(Date startDate, Date endDate);
-}
--- src/main/java/interfaces/IFileIO.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface IFileIO {
-
-    void saveAccountsToFile();
-
-    void loadAccountsFromFile();
-}
