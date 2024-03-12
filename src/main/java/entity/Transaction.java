777d5368e3cd024c32e87da718cd8679e90526d8
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-03-12 09:25:45 +0330

    Change transaction entity.

--- src/main/java/entity/Transaction.java
+++ src/main/java/entity/Transaction.java
@@ -7,42 +7,121 @@ import java.util.Date;
 
 public class Transaction implements Serializable {
 
+    private Long id;
+
     private TransactionType transactionType;
 
     private Date transactionDate;
 
     private double amount;
 
-    public Transaction(TransactionType transactionType, Date transactionDate, double amount) {
+    private double fee;
+
+    private BankAccount bankAccount;
+
+    public Transaction() {
+    }
+
+    public Transaction(TransactionType transactionType, Date transactionDate, double amount, BankAccount bankAccount) {
+        this(transactionType, transactionDate, amount, 0d, bankAccount);
+    }
+
+    public Transaction(TransactionType transactionType, Date transactionDate, double amount, double fee, BankAccount bankAccount) {
         this.transactionType = transactionType;
         this.transactionDate = transactionDate;
         this.amount = amount;
+        this.fee = fee;
+        this.bankAccount = bankAccount;
     }
 
     public Date getTransactionDate() {
         return transactionDate;
     }
 
-    public Transaction setTransactionDate(Date transactionDate) {
+    public void setTransactionDate(Date transactionDate) {
         this.transactionDate = transactionDate;
-        return this;
     }
 
     public double getAmount() {
         return amount;
     }
 
-    public Transaction setAmount(double amount) {
+    public void setAmount(double amount) {
         this.amount = amount;
-        return this;
     }
 
     public TransactionType getTransactionType() {
         return transactionType;
     }
 
-    public Transaction setTransactionType(TransactionType transactionType) {
+    public void setTransactionType(TransactionType transactionType) {
         this.transactionType = transactionType;
-        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    public BankAccount getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transaction that = (Transaction) o;
+
+        if (Double.compare(that.amount, amount) != 0) return false;
+        if (Double.compare(that.fee, fee) != 0) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (transactionType != that.transactionType) return false;
+        if (transactionDate != null ? !transactionDate.equals(that.transactionDate) : that.transactionDate != null)
+            return false;
+        return bankAccount != null ? bankAccount.equals(that.bankAccount) : that.bankAccount == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (transactionType != null ? transactionType.hashCode() : 0);
+        result = 31 * result + (transactionDate != null ? transactionDate.hashCode() : 0);
+        temp = Double.doubleToLongBits(amount);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(fee);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (bankAccount != null ? bankAccount.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", transactionType=" + transactionType +
+                ", transactionDate=" + transactionDate +
+                ", amount=" + amount +
+                ", fee=" + fee +
+                ", bankAccount=" + bankAccount +
+                '}';
     }
 }
