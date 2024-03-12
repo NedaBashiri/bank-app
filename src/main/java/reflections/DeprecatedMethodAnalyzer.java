29dcdeff22cc04c7d7ed19a6fc1e09ffb2f26aeb
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-03-12 09:47:20 +0330

    Add reflections library in project.

--- src/main/java/reflections/DeprecatedMethodAnalyzer.java
+++ src/main/java/reflections/DeprecatedMethodAnalyzer.java
@@ -4,30 +4,34 @@ import annotations.DeprecatedMethod;
 import entity.BankAccount;
 import entity.CheckingAccount;
 import entity.SavingsAccount;
+import org.reflections.Reflections;
 
-import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class DeprecatedMethodAnalyzer {
 
     public static void analyze() {
+        Reflections reflections = new Reflections("project.root.package");
+        Set<Class<?>> allClasses = reflections.getSubTypesOf(Object.class);
+
         List<Class<?>> classes = Arrays.asList(
                 BankAccount.class,
                 SavingsAccount.class,
                 CheckingAccount.class
         );
 
-        for (Class<?> clazz : classes) {
-            System.out.println("Class: " + clazz.getName());
-            for (Method method : clazz.getDeclaredMethods()) {
-                if (method.isAnnotationPresent(DeprecatedMethod.class)) {
+        classes.stream()
+                .flatMap(clazz -> Stream.of(clazz.getDeclaredMethods()))
+                .filter(method -> method.isAnnotationPresent(DeprecatedMethod.class))
+                .forEach(method -> {
                     DeprecatedMethod annotation = method.getAnnotation(DeprecatedMethod.class);
                     System.out.println("    Method: " + method.getName());
                     System.out.println("        Reason: " + annotation.reason());
                     System.out.println("        Replacement: " + annotation.replacement());
-                }
-            }
-        }
+                });
+
     }
 }
