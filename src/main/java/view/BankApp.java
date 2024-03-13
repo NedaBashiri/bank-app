4aca2903d595756b98bc673258d21ab61315dfdb
armanAsadi <armanasadi44@gmail.com>
2024-03-13 06:22:23 +0330

    using Logger.info instead of System.out.println at BankApp.java

--- src/main/java/view/BankApp.java
+++ src/main/java/view/BankApp.java
@@ -2,11 +2,8 @@ package view;
 
 import base.service.FileIO;
 import base.service.impl.FileService;
-import entity.BankAccount;
+import entity.*;
 
-import entity.CheckingAccount;
-import entity.SavingsAccount;
-import entity.Transaction;
 import repository.impl.BankAccountRepositoryImpl;
 import repository.impl.CheckingAccountRepositoryImpl;
 import repository.impl.SavingsAccountRepositoryImpl;
@@ -24,13 +21,16 @@ import javax.persistence.EntityManager;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class BankApp {
+
+    private static final Logger LOGGER = Logger.getLogger(BankApp.class.getName());
 
     public static void main(String[] args) {
 
         EntityManager entityManager = EntityManagerHelper.getEntityManager();
-
         try {
             BankAccount bankAccount  = new CheckingAccount("1235","account1",6000);
             BankAccount bankAccount1  = new SavingsAccount("1236","account2",4000);
@@ -53,43 +53,42 @@ public class BankApp {
             List<SavingsAccount> savingsAccounts =savingsAccountService.findAll(SavingsAccount.class);
             savingsAccountService.applyInterestConcurrently(2);
             Optional<CheckingAccount> bankAccount3 = checkingAccountService.findById(CheckingAccount.class, 1L);
-            System.out.println(bankAccount3.get());
+            LOGGER.info(bankAccount3.toString());
 
             EntityManagerHelper.commit();
 
             List<CheckingAccount> checkingAccounts = checkingAccountService.findAll(CheckingAccount.class);
-            System.out.println(checkingAccounts);
+            LOGGER.info(checkingAccounts.toString());
             List<SavingsAccount> accounts =savingsAccountService.findAll(SavingsAccount.class);
-            System.out.println(accounts);
+            LOGGER.info(accounts.toString());
 
 
             BankAccountService bankAccountService = new BankAccountServiceImpl(new BankAccountRepositoryImpl(entityManager),new BankAccountRepositoryImpl(entityManager),new TransactionServiceImpl(new TransactionRepositoryImpl(entityManager)));
 
             Optional byAccountNumber = bankAccountService.findByAccountNumber("1236");
-            System.out.println(byAccountNumber.get());
+            LOGGER.info(byAccountNumber.toString());
 
             List list = bankAccountService.searchAccountsWithBalanceGreaterThan(5000);
-            System.err.println(list);
+            LOGGER.info(list.toString());
 
             List all = bankAccountService.findAll(BankAccount.class);
             FileIO<BankAccount> bankAccountFileIO = new FileService();
             bankAccountFileIO.saveToFile(all,"accounts.txt");
             List<BankAccount> bankAccounts = bankAccountFileIO.loadFromFile("accounts.txt");
-            System.out.println(bankAccounts);
+            LOGGER.info(bankAccounts.toString());
 
             TransactionServiceImpl transactionService = new TransactionServiceImpl(new TransactionRepositoryImpl(entityManager));
             List<Transaction> byAccountId = transactionService.findByAccountId(2l);
-            System.out.println(byAccountId);
+            LOGGER.info(byAccountId.toString());
 
             List<Transaction> byAmountBetween = transactionService.findByAmountBetween(3000.0, 5000.0);
-            System.err.println(byAmountBetween);
+            LOGGER.info(byAmountBetween.toString());
 
             List<Transaction> transactions = transactionService.searchTransactionsInTimeInterval(new Date(2024 - 03 - 01), new Date());
-            System.out.println(transactions);
+            LOGGER.info(transactions.toString());
 
         } finally {
             EntityManagerHelper.closeEntityManager();
-
         }
     }
 
