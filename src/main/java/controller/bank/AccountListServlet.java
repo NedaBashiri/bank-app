13ff7501af62a4f278d75160cda40f7f15d04c4f
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-03-17 19:14:12 +0330

    Create account-list-servlet.

---
+++ src/main/java/controller/bank/AccountListServlet.java
@@ -0,0 +1,40 @@
+package controller.bank;
+
+
+
+import entity.BankAccount;
+import entity.Transaction;
+import repository.impl.BankAccountRepositoryImpl;
+import repository.impl.TransactionRepositoryImpl;
+import service.BankAccountService;
+import service.impl.BankAccountServiceImpl;
+import service.impl.TransactionServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class AccountListServlet extends HttpServlet {
+    private BankAccountService bankAccountService;
+
+    @Override
+    public void init() throws ServletException {
+        this.bankAccountService = new BankAccountServiceImpl(new BankAccountRepositoryImpl(BankAccount.class),new TransactionServiceImpl(new TransactionRepositoryImpl(Transaction.class)));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<BankAccount> accountList = bankAccountService.findAll();
+            req.setAttribute("list", accountList);
+            req.getRequestDispatcher("/bank/account.jsp").forward(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+            req.setAttribute("msg",e.getMessage());
+            resp.sendError(707);
+        }
+    }
+}
