b5e09589b3d1526f30f8b2b40215de742a43b6b5
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-03-17 19:21:51 +0330

    Create deposit-servlet.

---
+++ src/main/java/controller/bank/DepositServlet.java
@@ -0,0 +1,42 @@
+package controller.bank;
+
+import entity.BankAccount;
+import entity.Transaction;
+import repository.impl.BankAccountRepositoryImpl;
+import repository.impl.TransactionRepositoryImpl;
+import service.BankAccountService;
+import service.impl.BankAccountServiceImpl;
+import service.impl.TransactionServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+public class DepositServlet extends HttpServlet {
+    private BankAccountService bankAccountService;
+
+    @Override
+    public void init() throws ServletException {
+        this.bankAccountService = new BankAccountServiceImpl(new BankAccountRepositoryImpl(BankAccount.class),new TransactionServiceImpl(new TransactionRepositoryImpl(Transaction.class)));
+    }
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Long id = Long.valueOf(req.getParameter("id"));
+            Double amount = Double.valueOf(req.getParameter("amount"));
+            Optional bankAccountById = bankAccountService.findById(id);
+            BankAccount bankAccount = (BankAccount) bankAccountById.get();
+            bankAccountService.deposit(bankAccount,amount);
+            resp.sendRedirect("/bank/accountList");
+        } catch (Exception e) {
+            e.printStackTrace();
+            req.setAttribute("msg", e.getMessage());
+            resp.sendError(707);
+        }
+    }
+}
