69e5281fead608fa468f038a45ab7c0561813917
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-03-17 19:18:18 +0330

    Create account-update-servlet.

---
+++ src/main/java/controller/bank/AccountUpdateServlet.java
@@ -0,0 +1,41 @@
+package controller.bank;
+
+import entity.BankAccount;
+import entity.Transaction;
+import repository.impl.BankAccountRepositoryImpl;
+import repository.impl.TransactionRepositoryImpl;
+import service.BankAccountService;
+import service.impl.BankAccountServiceImpl;
+import service.impl.TransactionServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+public class AccountUpdateServlet extends HttpServlet {
+    private BankAccountService bankAccountService;
+
+    @Override
+    public void init() throws ServletException {
+        this.bankAccountService = new BankAccountServiceImpl(new BankAccountRepositoryImpl(BankAccount.class),new TransactionServiceImpl(new TransactionRepositoryImpl(Transaction.class)));
+    }
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Long id = Long.valueOf(req.getParameter("id"));
+            Optional bankAccountById = bankAccountService.findById(id);
+            BankAccount bankAccount = (BankAccount) bankAccountById.get();
+            req.setAttribute("account",bankAccount);
+            req.getRequestDispatcher("/bank/transaction.jsp").forward(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+            req.setAttribute("msg", e.getMessage());
+            resp.sendError(707);
+        }
+    }
+}
