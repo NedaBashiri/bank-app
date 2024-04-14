982df48bc8b8ebf64586816b17d33f06c7068e90
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-04-14 23:15:37 +0330

    Update account-list-servlet.

--- src/main/java/controller/bank/AccountListServlet.java
+++ src/main/java/controller/bank/AccountListServlet.java
@@ -4,27 +4,49 @@ package controller.bank;
 
 import entity.BankAccount;
 import entity.Transaction;
+import entity.User;
 import repository.impl.BankAccountRepositoryImpl;
 import repository.impl.TransactionRepositoryImpl;
+import repository.impl.UserRepositoryImpl;
 import service.BankAccountService;
+import service.UserService;
 import service.impl.BankAccountServiceImpl;
 import service.impl.TransactionServiceImpl;
+import service.impl.UserServiceImpl;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class AccountListServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        BankAccountService bankAccountService = new BankAccountServiceImpl(new BankAccountRepositoryImpl(BankAccount.class), new TransactionServiceImpl(new TransactionRepositoryImpl(Transaction.class)));
 
+        BankAccountService bankAccountService = new BankAccountServiceImpl(new BankAccountRepositoryImpl(BankAccount.class), new TransactionServiceImpl(new TransactionRepositoryImpl(Transaction.class)));
+        UserService userService = new UserServiceImpl(new UserRepositoryImpl(User.class));
+        Cookie[] cookies = req.getCookies();
+        String email = null;
+        if (cookies != null) {
+            Cookie emailCookie = Arrays.stream(cookies)
+                    .filter(cookie -> "email".equals(cookie.getName()))
+                    .findFirst()
+                    .orElse(null);
+
+            if (emailCookie != null) {
+                email = emailCookie.getValue();
+            }
+        }
+        Optional<User> userByEmail = userService.findUserByEmail(email);
+        User user = userByEmail.get();
         try {
-            List<BankAccount> accountList = bankAccountService.findAll();
+            List<BankAccount> accountList = bankAccountService.findAccountByUser(user);
             req.setAttribute("list", accountList);
             req.getRequestDispatcher("/bank/account.jsp").forward(req, resp);
         } catch (Exception e) {
